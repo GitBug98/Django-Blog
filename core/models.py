769effe68cd26c5bd035f12c9f257364e21df445
unknown <amir7510shalaby@gmail.com>
2023-08-01 04:50:21 +0300

    add basic posts model & register it

--- core/models.py
+++ core/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    posted_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from . models import Post
 
-# Create your views here.
+
 
 def home(request):
+    posts = Post.objects.all()
+    context = {"posts":posts}
     
-    return render (request, 'core/home.html')
+    return render (request, 'core/home.html',context)
 
 def about(request):
 
